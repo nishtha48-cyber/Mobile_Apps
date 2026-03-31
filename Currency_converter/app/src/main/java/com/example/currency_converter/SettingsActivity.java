9ac431018bd4aeedeb02070affcc2dd5f919eb7a
nishtha48-cyber <guptanishtha2006@gmail.com>
2026-03-31 19:47:14 +0530

    SettingsActivty updated

---
+++ Currency_converter/app/src/main/java/com/example/currency_converter/SettingsActivity.java
@@ -0,0 +1,29 @@
+package com.example.currencyconverter;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Switch;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Switch switchTheme = findViewById(R.id.switchTheme);
+        SharedPreferences pref = getSharedPreferences("prefs", MODE_PRIVATE);
+        boolean isDark = pref.getBoolean("darkTheme", false);
+        switchTheme.setChecked(isDark);
+
+        switchTheme.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putBoolean("darkTheme", isChecked);
+            editor.apply();
+
+            AppCompatDelegate.setDefaultNightMode(isChecked ?
+                    AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO);
+        });
+    }
+}
