72831a13334159b7e2a877d01712a66ca862fe75
nishtha48-cyber <guptanishtha2006@gmail.com>
2026-04-01 12:55:42 +0530

    Logic written

--- Sensors/app/src/main/java/com/example/sensors/MainActivity.java
+++ Sensors/app/src/main/java/com/example/sensors/MainActivity.java
@@ -1,24 +1,64 @@
 package com.example.sensors;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor accelerometer, lightSensor, proximitySensor;
+    private TextView tvAccel, tvLight, tvProximity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        tvAccel = findViewById(R.id.tvAccel);
+        tvLight = findViewById(R.id.tvLight);
+        tvProximity = findViewById(R.id.tvProximity);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (accelerometer != null) sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        if (lightSensor != null) sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        if (proximitySensor != null) sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        int sensorType = event.sensor.getType();
+        switch (sensorType) {
+            case Sensor.TYPE_ACCELEROMETER:
+                tvAccel.setText(String.format("X: %.2f | Y: %.2f | Z: %.2f", event.values[0], event.values[1], event.values[2]));
+                break;
+            case Sensor.TYPE_LIGHT:
+                tvLight.setText("Brightness: " + event.values[0] + " lx");
+                break;
+            case Sensor.TYPE_PROXIMITY:
+                tvProximity.setText("Distance: " + event.values[0] + " cm");
+                break;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+    
 }
