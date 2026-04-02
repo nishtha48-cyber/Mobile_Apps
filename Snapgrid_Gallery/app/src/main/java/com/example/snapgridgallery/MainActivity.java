6c8a7f99ea2bd73cc70223e4e93a5199ca50b0d4
nishtha48-cyber <guptanishtha2006@gmail.com>
2026-04-02 11:23:23 +0530

    layout made

--- Snapgrid_Gallery/app/src/main/java/com/example/snapgridgallery/MainActivity.java
+++ Snapgrid_Gallery/app/src/main/java/com/example/snapgridgallery/MainActivity.java
@@ -9,16 +9,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    File photoFile;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        findViewById(R.id.btnCapture).setOnClickListener(v -> dispatchTakePictureIntent());
+        findViewById(R.id.btnGallery).setOnClickListener(v ->
+                startActivity(new Intent(this, GalleryActivity.class)));
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        try {
+            photoFile = File.createTempFile("IMG_" + timeStamp, ".jpg", storageDir);
+            Uri photoURI = FileProvider.getUriForFile(this, getPackageName() + ".provider", photoFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch(IOException ex) { ex.printStackTrace(); }
     }
 }
